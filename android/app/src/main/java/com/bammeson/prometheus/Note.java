e7b3effb724b05b5d84f2f7281f1b0a955d5e4ac
Brett Ammeson <ammesonb@gmail.com>
2015-07-05 21:20:49 -0400

    Makes notes display and adds onclick to enable editing

--- android/app/src/main/java/com/bammeson/prometheus/Note.java
+++ android/app/src/main/java/com/bammeson/prometheus/Note.java
@@ -6,14 +6,33 @@ import java.util.Date;
  * Created by brett on 7/3/15.
  * Represents a note object
  */
-public class Note {
+public class Note implements Comparable {
+    private int id;
     private String title;
     private String text;
     private Date lastModified;
 
-    public Note(String title, String text, Date lastModified) {
+    public Note(int id, String title, String text, Date lastModified) {
+        this.id = id;
         this.title = title;
         this.text = text;
         this.lastModified = lastModified;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(Object n) {
+        return this.title.compareTo(((Note) n).getTitle());
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- android/app/src/main/java/com/bammeson/prometheus/NotesActivity.java
+++ android/app/src/main/java/com/bammeson/prometheus/NotesActivity.java
@@ -1,16 +1,22 @@
 package com.bammeson.prometheus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class NotesActivity extends Activity {
+    ArrayList<Note> notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +30,27 @@ public class NotesActivity extends Activity {
             return;
         }
 
-        ArrayList<Note> notes = session.getNotes();
+        notes = session.getNotes();
+        Collections.sort(notes);
+        ArrayList<String> noteTitles = new ArrayList<String>();
+        for (Note n : notes) {
+            noteTitles.add(n.getTitle());
+        }
         ListView noteList = (ListView)findViewById(R.id.notesList);
-        // TODO set adapter and load it using returned data
-        //noteList.setAdapter();
-        // TODO add onclick handler to go to note selection screen
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, noteTitles);
+        noteList.setAdapter(adp);
+
+        noteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                Intent i = new Intent(getApplicationContext(), NoteEditActivity.class);
+                i.putExtra("id", notes.get(pos).getId());
+                i.putExtra("title", notes.get(pos).getTitle());
+                i.putExtra("text", notes.get(pos).getText());
+                startActivity(i);
+            }
+        });
     }
 
     @Override
